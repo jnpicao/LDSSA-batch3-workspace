136db206a42d283c4fa507cfe77782a2ffaf29fe
Joao N. Picao <jnpicaooliveira@gmail.com>
2020-01-15 19:45:32 +0000

    Introduce folder to LDSSA team

---
+++ Capstone - LDSSA tem/heroku-app-model-deploy/app.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 26 12:34:26 2019
+
+@author: jnpicao
+"""
+
+# the request object does exactly what the name suggests: holds
+# all of the contents of an HTTP request that someone is making
+# the Flask object is for creating an HTTP server - you'll
+# see this a few lines down.
+# the jsonify function is useful for when we want to return
+# json from the function we are using.
+
+import os
+import json
+import pickle
+import pandas as pd
+from sklearn.externals import joblib
+from flask import Flask, request, jsonify
+from peewee import (
+    SqliteDatabase, PostgresqlDatabase, Model, IntegerField,
+    FloatField, BooleanField, TextField, IntegrityError
+)
+from playhouse.shortcuts import model_to_dict
+
+########################################
+# Begin database stuff
+
+
+if 'DATABASE_URL' in os.environ:
+    db_url = os.environ['DATABASE_URL']
+    dbname = db_url.split('@')[1].split('/')[1]
+    user = db_url.split('@')[0].split(':')[1].lstrip('//')
+    password = db_url.split('@')[0].split(':')[2]
+    host = db_url.split('@')[1].split('/')[0].split(':')[0]
+    port = db_url.split('@')[1].split('/')[0].split(':')[1]
+    DB = PostgresqlDatabase(
+        dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+    )
+else:
+    DB = SqliteDatabase('predictions.db')
+
+
+class Prediction(Model):
+    observation_id = IntegerField(unique=True)
+    observation = TextField()
+    proba = FloatField()
+    true_class = IntegerField(null=True)
+
+    class Meta:
+        database = DB
+
+DB.create_tables([Prediction], safe=True)
+
+# End database stuff
+########################################
+
+
+########################################
+# Unpickle the previously-trained model 
+
+with open('columns.json') as fh:
+    columns = json.load(fh)
+
+with open('pipeline.pickle', 'rb') as fh:
+    pipeline = joblib.load(fh)
+    
+with open('dtypes.pickle', 'rb') as fh:
+    dtypes = pickle.load(fh)
+
+# End model un-pickling
+########################################
+
+
+########################################
+# Begin webserver stuff
+
+# here we use the Flask constructor to create a new
+# application that we can add routes to
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    obs_dict = request.get_json()
+    
+    _id = obs_dict['id']
+    
+    observation = obs_dict['observation']
+    obs = pd.DataFrame([observation], columns=columns).astype(dtypes)
+    proba = pipeline.predict_proba(obs)[0, 1]
+    
+    if proba > 0.5:
+        response_val = 1
+    else:
+        response_val = 0
+    
+    response = {"ContrabandIndicator": response_val}
+    p = Prediction(
+        observation_id=_id,
+        proba=proba,
+        observation=request.data,
+    )
+    
+    try:
+        p.save()
+    except IntegrityError:
+        error_msg = "ERROR: Observation ID: '{}' already exists".format(_id)
+        response["error"] = error_msg
+        print(error_msg)
+        DB.rollback()
+    return jsonify(response)
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    obs = request.get_json()
+    try:
+        p = Prediction.get(Prediction.observation_id == obs['id'])
+        p.true_class = obs['true_class']
+        p.save()
+        return jsonify(model_to_dict(p))
+    except Prediction.DoesNotExist:
+        error_msg = 'Observation ID: "{}" does not exist'.format(obs['id'])
+        return jsonify({'error': error_msg})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
